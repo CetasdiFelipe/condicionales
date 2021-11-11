d8ec94c132c07aa90d20ab071086af9b49e166de
FelipeCiro <78991585+FelipeCiro@users.noreply.github.com>
2021-11-10 20:26:22 -0500

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,6 @@
+let variable_a_comparar = 50;
+if (variable_a_comparar > 100) {
+    alert("El valor de la variable es mayor que 100");
+} else {
+    alert("El valor de la variable es menor o igual que 100");
+}
---
+++ ejercicio2.js
@@ -0,0 +1,6 @@
+const estado = 200;
+if (estado === 400) {
+    console.log("OK!");
+} else {
+    console.log("status desconocido");
+}
